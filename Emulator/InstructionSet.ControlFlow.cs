4452045a93c620f3ada7ea379dc418f38303c221
iuculano <82924525+iuculano@users.noreply.github.com>
2021-07-15 01:22:06 -0400

    Split up the helper functions. This is purely organizational.

---
+++ Emulator/InstructionSet.ControlFlow.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+
+namespace axGB.CPU
+{
+    public partial class InstructionSet
+    {
+        private void Call(ushort address)
+        {
+            Push(processor.registers.PC);
+            processor.registers.PC = address;
+        }
+
+        private void Call(ushort address, Flags flags, bool condition)
+        {
+            if (HasFlags(flags) == condition)
+            {
+                Call(address);
+            }
+
+            // Pay the conditional tax
+            processor.cycles += 12;
+        }
+
+        private void Ret(Flags flags, bool condition)
+        {
+            if (HasFlags(flags) == condition)
+            {
+                processor.registers.PC = Pop();
+            }
+
+            processor.cycles += 12;
+        }
+
+        private void Reti()
+        {
+            processor.interuptHandler.IME = true;
+            processor.registers.PC        = Pop();
+        }
+
+        private void Jump(ushort value)
+        {
+            processor.registers.PC = value;
+        }
+
+        private void Jump(ushort value, Flags flags, bool condition)
+        {
+            if (HasFlags(flags) == condition)
+            {
+                Jump(value);
+            }
+
+            // Pay the conditional tax
+            processor.cycles += 4;
+        }
+
+        private void JumpRelative(byte value)
+        {
+            // value can be negative here, it's possible to jump backwards
+            var pc                 = (int)(processor.registers.PC) + (sbyte)value;
+            processor.registers.PC = (ushort)pc;
+
+            return;
+        }
+
+        private void JumpRelative(byte value, Flags flags, bool condition)
+        {
+            if (HasFlags(flags) == condition)
+            {
+                JumpRelative(value);
+            }
+
+            // Pay the conditional tax
+            processor.cycles += 4;
+        }
+
+        private void Cp(byte value)
+        {
+            var result = processor.registers.A - value;
+
+            var zero   = (byte)result == 0;
+            var half   = ((processor.registers.A & 0x0F) - (value & 0x0F)) < 0;
+            var carry  = result < 0;
+
+            SetFlags(Flags.Zero,      zero);
+            SetFlags(Flags.Subtract,  true);
+            SetFlags(Flags.HalfCarry, half);
+            SetFlags(Flags.Carry,     carry);
+        }
+
+        private void Rst(ushort value)
+        {
+            Call(value);
+        }
+    }
+}
---
+++ Emulator/InstructionSet.Misc.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace axGB.CPU
+{
+    public partial class InstructionSet
+    {
+        private void Scf()
+        {
+            SetFlags(Flags.Carry,     true);
+            SetFlags(Flags.Subtract,  false);
+            SetFlags(Flags.HalfCarry, false);
+        }
+
+        private void Ccf()
+        {
+            SetFlags(Flags.Carry,     !HasFlags(Flags.Carry));
+            SetFlags(Flags.Subtract,  false);
+            SetFlags(Flags.HalfCarry, false);
+        }
+
+        private void Ei()
+        {
+            processor.interuptHandler.IME          = true;
+            processor.interuptHandler.NeedsEIDelay = true;
+
+
+            var opcode = processor.memory.ReadByte(processor.registers.PC);
+            var length = Opcodes[opcode].OperandLength + 1;
+            Push((ushort)(processor.registers.PC + length));
+        }
+    }
+}
