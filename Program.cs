b620e683fa3087c35ce3e79c5824d1fb457b1062
iuculano <82924525+iuculano@users.noreply.github.com>
2023-11-24 20:44:36 -0500

    Start pulling in bits to actually create a proper window instead of using a hack to draw over a console

--- Program.cs
+++ Program.cs
@@ -1,34 +1,70 @@
 ﻿using axGB.CPU;
 using axGB.System;
 
+using Silk.NET.Input;
+using Silk.NET.Maths;
+using Silk.NET.Windowing;
+
 namespace axGB
 {
     class Program
     {
+        private static IWindow window;
+        private static double  accumulator;
+
+        private static MemoryBus         memory;
+        private static Processor         processor;
+        private static GraphicsProcessor graphics;
+        private static Timer             timer;
+       
+
+        private static void OnLoad()
+        {
+            memory    = new MemoryBus();
+            processor = new Processor(memory);
+            timer     = new Timer(memory);
+            graphics  = new GraphicsProcessor(memory);
+
+            var cartridge = Cartridge.Load(@"01-special.gb", memory);
+            memory.Connect(cartridge);
+        }
+
+        private static void OnUpdate(double deltaTime)
+        {
+            accumulator += deltaTime;
+
+            var cycles          = 0;
+            var cyclesThisFrame = 0;
+
+            while (cyclesThisFrame < Processor.CyclesPerFrame)
+            {
+                cycles = processor.Update();
+                timer.Update(cycles);
+                graphics.Update(cycles);
+
+                cyclesThisFrame += cycles;
+            }
+        }
+
+        private static void KeyDown(IKeyboard arg1, Key arg2, int arg3)
+        {
+            if (arg2 == Key.Escape)
+            {
+                window.Close();
+            }
+        }
+
         static void Main(string[] args)
         {
-            var memory    = new MemoryBus();
-            var processor = new Processor(memory);
-            var graphics  = new GraphicsProcessor(memory);
-            var cartridge = Cartridge.Load(@"09-op r,r.gb", memory);  // 4, 5, 6
+            var options   = WindowOptions.Default;
+            options.Size  = new Vector2D<int>(800, 600);
 
-            memory.Connect(cartridge);
+            window         = Window.Create(options);
+            window.Load   += OnLoad;
+            window.Update += OnUpdate;
 
-            while(true)
-            {
-                var cycles          = 0;
-                var cyclesThisFrame = 0;
-
-                while (cyclesThisFrame < Processor.CyclesPerFrame)
-                {                    
-                    cycles = processor.Update();
-                    graphics.Update(cycles);
-
-                    cyclesThisFrame += cycles;
-                }
-
-                // Thread.Sleep(16);
-            }
+            window.Run();
+            window.Dispose();
         }
     }
 }
