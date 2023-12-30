845546b1b7dde71dd5b0869d70f2083d9e545332
iuculano <82924525+iuculano@users.noreply.github.com>
2023-12-30 01:34:39 -0500

    Rewrite the background rendering to iterate pixels in a scanline instead of tile chunks and fix  numerous graphics issues with it.
    Test roms actually draw their text now - finally, and Pokemon isn't quite as much of a garbled mess

--- Emulator/GraphicsProcessor.cs
+++ Emulator/GraphicsProcessor.cs
@@ -75,39 +75,98 @@ namespace axGB.CPU
             }
         }
 
-        public void WalkTileMapRow(int scanline)
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public byte DecodeTileData(ReadOnlySpan<byte> data, int column, int scanline)
+        {
+            // https://www.huderlem.com/demos/gameboy2bpp.html
+
+            // 2 bytes make up an entire row - 16 bytes for the entire tile
+            // Grab the entire row we're on first
+            // y is multiplied to compensate for this, for example:
+            // 0 * 2 = 0 - byte 0 and 1 comprise a row
+            // 1 * 2 = 2 - byte 2 and 3 comprise a row
+            // 2 * 2 = 4 - byte 4 and 5 comprise a row...
+            var rowIndex = scanline * 2;       // This saves an instruction somehow
+            var one      = data[rowIndex];     // byte 1
+            var two      = data[rowIndex + 1]; // byte 2
+            
+            // Mask to grab just the current bit handling
+            var bit = 0b_10000000 >> column;
+            
+            // 2 bits per pixel, 0b_000000HL
+            // Shift into position depending on which bit we're on
+            var low  = (one & bit) > 0 ? 0b_00000001 : 0; // 0b_0000000L
+            var high = (two & bit) > 0 ? 0b_00000010 : 0; // 0b_000000H0
+            return (byte)(high | low);                    // 0b_000000HL
+        }
+
+        public void WalkScanline(int scanline)
         {
             // https://gbdev.io/pandocs/LCDC.html
-
-            // Memory locations can differ depending how the register is set
-            var tileData = (memory.LCDC & (byte)LCDControl.BackgroundDataArea)    == 0 ? 0x8800 : 0x8000;
-            var tileMap  = (memory.LCDC & (byte)LCDControl.BackgroundTileMapArea) == 0 ? 0x9800 : 0x9C00;
-            var window   = (memory.LCDC & (byte)LCDControl.WindowEnabled)         == 0;
-
-            tileData = tileData - 0x8000;
-            tileMap  = tileMap  - 0x8000;
-
-            // Need to figure out what row needs to be drawn, and what line from within the tiles
-            // scanline will be the LY register, so LY + where the 
-            var scanlineToTileRow = ((scanline + memory.SCY) * 18) / ScreenHeight;
-            var tileScanline      =  (scanline + memory.SCY) % 8;
-
-            // Walk through the tiles in a row
-            for (var x = 0; x < 20; x++)
+            var lcdc = (LCDControl)memory.LCDC;
+            if (lcdc.HasFlag(LCDControl.BackgroundWindowEnable))
             {
-                var viewY = scanlineToTileRow;
-                var viewX = x + memory.SCX;
+            // Memory locations can differ depending how the register is set
+                var tileDataOffset = lcdc.HasFlag(LCDControl.BackgroundDataArea)    ? 0x0000 : 0x0800;
+                var tileMapOffset  = lcdc.HasFlag(LCDControl.BackgroundTileMapArea) ? 0x1C00 : 0x1800;
+                var window         = lcdc.HasFlag(LCDControl.WindowEnabled)         ? 0x0000 : 0x0000;
 
-                // The background tile map is 32x32
-                var index = (viewY * 32) + viewX;
-                var id    = memory.VRAM[tileMap + index];
+                for (var x = 0; x < ScreenWidth; x++)
+                {
+                    // Figure out which pixel we should be drawing out of the 256x256 background
+                    // As we draw the entire scanline at once, we just need to add the scroll registers
+                    // to get the correct offset
+                    byte backgroundX = (byte)(x + memory.SCX);
+                    byte backgroundY = (byte)(scanline + memory.SCY);
 
-                // Each tile is 16 bytes, so finding the right tile and
-                // multiplying by 16 will point at the right spot in memory
-                var ptr   = tileData + (id * 16);
-                var span  = new ReadOnlySpan<byte>(memory.VRAM, ptr, 16);
+                    // Since tiles are 8x8, we just divide the coordinates in background/pixel space
+                    // by 8 to find the correct tile coordinate in the 32x32 tile grid
+                    var tileX = (backgroundX / 8);
+                    var tileY = (backgroundY / 8);                    
 
-                DrawTileScanline(span, x, tileScanline, scanline);
+                    // Now we need to find the appropriate entry in the tile map (which is 32x32)
+                    // Each entry is 1 byte - pretend like we're indexing into a 2D array: (y * width) + x
+                    var offset = (tileY * 32) + tileX;
+                    var index  = tileMapOffset + offset;
+
+                    // https://gbdev.io/pandocs/Tile_Data.html#vram-tile-data
+                    byte id = 0;
+                    if (tileDataOffset == 0)
+            {
+                        id  = memory.VRAM[index];
+                    }
+
+                    // Need to use signed addressing if LCDControl.BackgroundDataArea is set
+                    else
+                    {
+                        // Not sure if this is actually right???
+                        // Something seems to be off, 0x9000 tiles don't seem to render right
+                        id  = (byte)(memory.VRAM[index] + 128);
+                    }
+
+                    // Get the memory location of the appropriate tile's actual graphic data
+                    // Each tile is 16 bytes, so the offset is just id * 16
+                    var ptr  = tileDataOffset + (id * 16);
+                    var data = new ReadOnlySpan<byte>(memory.VRAM, ptr, 16);
+
+                    // Figure out where we are in the tile - each tile is 8x8 pixels
+                    // Just wrap with modulo:
+                    // 0 - 7
+                    // 8 - 15
+                    // For example, an x of 8 is pixel 0 in the second tile
+                    var tileDataColumn   = backgroundX % 8;
+                    var tileDataScanline = backgroundY % 8;
+
+                    // Get the color for the pixel in the tile, it's just an index into the palette
+                    var color = DecodeTileData(data, tileDataColumn, tileDataScanline);
+                    Backbuffer[(scanline * ScreenWidth) + x] = palette[color];
+
+                }                
+            }
+
+            else
+            {
+                Console.WriteLine("Background disabled - sprite drawing stubbed.");
             }
         }
 
@@ -210,7 +269,7 @@ namespace axGB.CPU
                 case Mode.Transfer:
                     if (this.cycles >= 172)
                     {
-                        WalkTileMapRow(memory.LY);
+                            WalkScanline(memory.LY);
 
                         memory.STAT  = (int)Mode.HBlank;
                         this.cycles -= 172;
