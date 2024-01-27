bf5ba72e1ad754136fe8e1c68ebb36a7a9bf81fd
iuculano <82924525+iuculano@users.noreply.github.com>
2024-01-27 04:03:32 -0500

    Timer cleanup

--- src/Emulator.Core/Bus/Timer.cs
+++ src/Emulator.Core/Bus/Timer.cs
@@ -4,6 +4,8 @@ namespace Emulator.Core.Bus;
 
 public class Timer
 {
+    // https://gbdev.io/pandocs/Timer_and_Divider_Registers.html
+
     private readonly MemoryBus        memory;
     private readonly InterruptHandler interruptHandler;
     private readonly int[]            tacFrequencyTable = { 1024, 16, 64, 256 };
@@ -17,40 +19,40 @@ public class Timer
         this.interruptHandler = interruptHnadler;
     }
 
+    private bool IsEnabled
+    {
+        get => (memory.TAC & 0b_00000100) == 0b_00000100;
+    }
+
+    private int Frequency
+    {
+        get => tacFrequencyTable[memory.TAC & 0b_00000011];
+    }
+
     public void Update(int cycles)
     {
-        // https://gbdev.io/pandocs/Timer_and_Divider_Registers.html
         divAccumulator  += cycles;
         timaAccumulator += cycles;
 
-        // Update DIV - note that it always ticks regardless of TAC's state
+        // DIV always ticks regardless of TAC's state
         // 4194304 cycles / 16384 = 256, Divider updates every 256 cycles
         if (divAccumulator >= 256)
         {
-            // Directly increment this register without writing through the
-            // memory bus anything else trying to write to that register should
-            // simply set it to 0
             memory.DIV++;
 
-            // Don't blindly set divAccumulator to zero, there's no guarantee we
-            // have moved exactly 256 cycles - divAccumulator may be something
-            // like 260 for example and DIV would wind up incrementing at the
-            // wrong pace
+            // There's no guarantee we have churned exactly 256 cycles - simply
+            // setting this to 0 can be wrong timing wise
             divAccumulator -= 256;
         }
 
         // Check if the timer control is actually enabled
-        if ((memory.TAC & 0b_00000100) == 0b_00000100)
+        if (IsEnabled)
         {
             // Update TIMA register
-            // The frequency of which it's updated depends on what's in the TAC
-            // register
-            var frequency = tacFrequencyTable[memory.TAC & 0b_00000011];
-
-            // If we've consumed enough cycles to tick
-            if (timaAccumulator >= frequency)
+            if (timaAccumulator >= Frequency)
             {
-                var pendingValue = memory.TIMA + 1; // Check for overflow
+                // Check for overflow
+                var pendingValue = memory.TIMA + 1;
                 if (pendingValue > 0xFF)
                 {
                     memory.TIMA = memory.TMA;
@@ -62,7 +64,7 @@ public class Timer
                     memory.TIMA++;
                 }
 
-                timaAccumulator -= frequency;
+                timaAccumulator -= Frequency;
             }
         }
     }
